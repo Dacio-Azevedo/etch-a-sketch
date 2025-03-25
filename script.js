1f704c4242033ad799967428262273bef1f38d6a
Dacio Azevedo <dacioherazan2@gmail.com>
2025-03-25 19:53:03 -0300

    Criação da grade e evento de mudança de cores

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("container-divs")
+    container.appendChild(div);
+
+    div.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = `#${randomColor()}`;
+
+    });
+}
+
+function randomColor() {
+    console.log(Math.floor(Math.random() * 16777215).toString(16));
+    return Math.floor(Math.random() * 16777215).toString(16);
+}
