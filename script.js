f2d5ac1c81ec0d1be4094f62094e46ded7a427a9
Dacio Azevedo <dacioherazan2@gmail.com>
2025-03-25 20:37:43 -0300

    Mudança na grade e adição de botão

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
     div.classList.add("container-divs")
     container.appendChild(div);
 
     div.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = `#${randomColor()}`;
+
+    });
+
+    div.addEventListener("mouseout", (event) => {
         event.target.style.backgroundColor = `#${randomColor()}`;
 
     });
