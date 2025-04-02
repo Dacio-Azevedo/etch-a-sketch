6dcc1337b6bff80dfd4d7151d676137b185e333f
Dacio Azevedo <dacioherazan2@gmail.com>
2025-04-02 14:10:59 -0300

    Estilização dinâmica da altura e largura do conteudo

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+const gridSize = document.querySelector("button");
+
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("container-divs")
-    container.appendChild(div);
+function generateGrid(row, total) {
 
-    div.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = `#${randomColor()}`;
+    container.innerHTML = "";
 
-    });
+    for (let i = 1; i <= total; i++) {
+        const div = document.createElement("div");
+        div.classList.add("container-divs");
+        div.style.width = `calc(100%/${row})`;
+        div.style.height = `calc(100%/${row})`;
+        container.appendChild(div);
 
-    div.addEventListener("mouseout", (event) => {
-        event.target.style.backgroundColor = `#${randomColor()}`;
+        div.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = `#${randomColor()}`;
 
-    });
+        });
+
+        div.addEventListener("mouseout", (event) => {
+            event.target.style.backgroundColor = `#${randomColor()}`;
+
+        });
+    }
 }
 
 function randomColor() {
-    console.log(Math.floor(Math.random() * 16777215).toString(16));
+    // console.log(Math.floor(Math.random() * 16777215).toString(16));
     return Math.floor(Math.random() * 16777215).toString(16);
 }
+
+gridSize.addEventListener("click", () => {
+    const divsPerRow = parseInt(prompt("Enter the size of the grid: ", ""));
+    const totalDivs = divsPerRow * divsPerRow;
+
+    generateGrid(divsPerRow, totalDivs);
+
+})
