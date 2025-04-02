9b8517efaba11d8feb1f1b222fb83418124f0a20
Dacio Azevedo <dacioherazan2@gmail.com>
2025-04-02 14:20:39 -0300

    Mudança em nome de variável

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ const gridSize = document.querySelector("button");
 
 const container = document.querySelector("#container");
 
-function generateGrid(row, total) {
+function generateGrid(side, total) {
 
     container.innerHTML = "";
 
-    for (let i = 1; i <= total; i++) {
-        const div = document.createElement("div");
-        div.classList.add("container-divs");
-        div.style.width = `calc(100%/${row})`;
-        div.style.height = `calc(100%/${row})`;
-        container.appendChild(div);
+    if (side <= 100) {
+        for (let i = 1; i <= total; i++) {
+            const div = document.createElement("div");
+            div.classList.add("container-divs");
+            div.style.width = `calc(100%/${side})`;
+            div.style.height = `calc(100%/${side})`;
+            container.appendChild(div);
 
-        div.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = `#${randomColor()}`;
+            div.addEventListener("mouseover", (event) => {
+                event.target.style.backgroundColor = `#${randomColor()}`;
 
-        });
+            });
 
-        div.addEventListener("mouseout", (event) => {
-            event.target.style.backgroundColor = `#${randomColor()}`;
+            div.addEventListener("mouseout", (event) => {
+                event.target.style.backgroundColor = `#${randomColor()}`;
 
-        });
+            });
+        }
+    }
+    else {
+        alert("The grid size must no be longer than 100. Try again with a lower number.");
     }
 }
 
@@ -31,9 +36,9 @@ function randomColor() {
 }
 
 gridSize.addEventListener("click", () => {
-    const divsPerRow = parseInt(prompt("Enter the size of the grid: ", ""));
-    const totalDivs = divsPerRow * divsPerRow;
+    const divsPerSide = parseInt(prompt("Enter the size of the grid: ", ""));
+    const totalDivs = divsPerSide * divsPerSide;
 
-    generateGrid(divsPerRow, totalDivs);
+    generateGrid(divsPerSide, totalDivs);
 
 })
